ee8658010092292f8e8a07593cf097606b186ba7
Felix Nößler <felixnoessler@aol.com>
2024-07-16 20:44:45 +0200

    update main figure

--- julia_scripts/scripts/02_dS_dI_plot.jl
+++ julia_scripts/scripts/02_dS_dI_plot.jl
@@ -5,9 +5,9 @@ using JLD2
 let
     @info "Create figure 2"
     ##############################
-    ti_attack = [1,        1.1,      1.2,      1.25,     1.3,      1.4,      1.5]
-    ti1 = [0.195527, 0.217102, 0.236287, 0.244931, 0.252933, 0.26703,  0.278672]
-    ti2 = [0.08636,  0.083894, 0.082123, 0.081458, 0.080921, 0.080188, 0.07984 ]
+    ti_attack = [1.0, 1.3]
+    ti1 = [0.308134,  0.252933]
+    ti2 = [0.048367,  0.080921]
     ##############################
 
     sim_result = load("simulation_results/02_dS_dI_pattern.jld2")
@@ -55,18 +55,18 @@ let
     xticklabels = ["     10⁻³", "10⁻²", "10⁻¹", "10⁰", "10¹   "]
     yticklabels = ["10⁻³", "10⁻²", "10⁻¹", "10⁰", "10¹"]
 
-    Label(fig[0, 1:2], "Self-organised pattern formation",
+    Label(fig[1, 2], "Self-organised pattern formation",
         font = "TeX Gyre Heros Makie Bold", fontsize = 22)
-    Label(fig[1, 1], "osc. TI"; halign = :left)
-    Label(fig[1, 1], "static TI"; halign = :right)
-    Label(fig[1, 1], "          no TI"; halign = :center)
+    Label(fig[2, 2], "osc. TI"; halign = :left)
+    Label(fig[2, 2], "static TI"; halign = :right)
+    Label(fig[2, 2], "          no TI"; halign = :center)
 
-    Label(fig[2, 2], "static TI"; valign = :top, rotation = -pi/2)
-    Label(fig[2, 2], "osc. TI"; valign = :bottom, rotation = -pi/2)
-    Label(fig[2, 2], "no TI        "; valign = :center, rotation = -pi/2)
+    Label(fig[3, 3], "static TI"; valign = :top, rotation = -pi/2)
+    Label(fig[3, 3], "osc. TI"; valign = :bottom, rotation = -pi/2)
+    Label(fig[3, 3], "no TI        "; valign = :center, rotation = -pi/2)
 
 
-    Axis(fig[2, 1];
+    Axis(fig[3, 2];
         width = axis_size, height = axis_size,
         topspinevisible = false,
         rightspinevisible = false,
@@ -79,9 +79,8 @@ let
         yminorticksvisible = true,
         yminorticks = IntervalsBetween(9),
         xgridvisible = false, ygridvisible = false,
-        limits = (10 ^ -2.5, 10 ^ 0.5, 10 ^ - 2.5,  10 ^ 0.5),
-        xlabel = "dispersal rate of the superior competitor dS",
-        ylabel = "dispersal rate of the inferior competitor dI")
+        limits = (10 ^ -2.5, 10 ^ 0.5, 10 ^ - 2.5,  10 ^ 0.5))
+
     heatmap!(
         dS, dI,
         osc_coexistence',
@@ -102,7 +101,7 @@ let
     vlines!(ti2[findfirst(ti_attack .== 1.3)]; color = :black, linestyle = :dash)
     hlines!(ti1[findfirst(ti_attack .== 1.0)]; color = :black, linestyle = :dot)
     hlines!(ti2[findfirst(ti_attack .== 1.0)]; color = :black, linestyle = :dot)
-    text!([1e-2, 1], [0.45, 1]; text = ["Bet hedging", "Maladaptive\ndispersal"],
+    text!([1e-2, 1], [0.45, 1]; text = ["bet-hedging", "maladaptive\ndispersal"],
         align = (:center, :center))
     text!([0.0035], [3]; text = "A",
         fontsize = 26,
@@ -110,23 +109,37 @@ let
         align = (:left, :top),
         color = :black)
 
-    Legend(fig[1:2, 3],
-           [[MarkerElement(color = (:black, 0.3), marker = :rect, markersize = 30,
-                markerstrokewidth = 1.5, markerstrokecolor = :red)],
-           [MarkerElement(color = :lightblue, marker = :rect, markersize = 30),
+    ##############################
+    #A: d_S=0.005, d_I=0.018, B : d_S=1, d_I=0.47, C: d_S=0.005, d_I=0.1, D: d_S=1, d_I=0.13.
+    markersize = 20; color = :black; strokecolor = :white; strokewidth = 1;
+    scatter!([0.005], [0.1]; color, strokecolor, strokewidth, markersize, marker = :rect)
+    scatter!([0.005], [0.018]; color, strokecolor,strokewidth,  markersize, marker = :pentagon)
+    scatter!([1], [0.47]; color, strokecolor, strokewidth, markersize, marker = :diamond)
+    scatter!([1], [0.13]; color, strokecolor, strokewidth, markersize, marker = :utriangle)
+    ##############################
+
+    legend_layout = GridLayout(fig[3, 4]; valign = :top)
+    Legend(legend_layout[1, 1],
+        [MarkerElement(color = (:black, 0.3), marker = :rect, markersize = 30,
+            markerstrokewidth = 1.5, markerstrokecolor = :red)],
+        ["Coexistence only possible\nwith self-organised\npattern formation due to\nheterogeneity modulation"],
+        framevisible = false, ,
+        patchlabelgap = 10, tellheight = true)
+    Legend(legend_layout[2, 1],
+           [[MarkerElement(color = :lightblue, marker = :rect, markersize = 30),
            MarkerElement(color = :blue, marker = :rect, markersize = 30)],
            [LineElement(linestyle = :dash), LineElement(linestyle = :dot)]],
-           [["Coexistence only possible\nwith self-organised\npattern formation"],
-           ["with static dynamic", "with oscillatory dynamic"],
+           [["with static dynamics", "with oscillatory dynamics"],
             ["of superior competitor",
             "of inferior competitor"]],
-            ["", "Coexistence", "Turing boundaries"],
-           framevisible = true,
-           gridshalign = :left, valign = :top,
+            ["Coexistence", "Turing boundaries"],
+           framevisible = false,
+           gridshalign = :left,
            titlehalign = :left,
-           groupgap = 20, rowgap = 5, patchlabelgap = 10)
+           groupgap = 20, rowgap = 5, patchlabelgap = 10, tellheight = true)
+    Box(legend_layout[1:2, 1], color = :transparent, strokecolor = :black)
 
-    Axis(fig[2, 2:3];
+    Axis(fig[3, 3:4];
         title = "Environmental habitat\nheterogeneity",
         alignmode = Outside(),
         width = axis_size / 3, height = axis_size / 3,
@@ -163,15 +176,36 @@ let
         align = (:left, :top),
         color = :black)
 
+
+    ylabellayout = GridLayout(fig[3, 1])
+    xlabellayout = GridLayout(fig[4, 2])
+
+    Label(ylabellayout[2, 1], "dispersal rate of the inferior competitor",
+        rotation = pi/2)
+    Label(ylabellayout[1, 1], L"d_I",
+        rotation = pi/2)
+    Label(xlabellayout[1, 1], "dispersal rate of the superior competitor")
+    Label(xlabellayout[1, 2], L"d_S")
+
     rowgap!(fig.layout, 1, 0)
     rowgap!(fig.layout, 2, 0)
-    colgap!(fig.layout, 1, 0)
-    colgap!(fig.layout, 2, 25)
+    rowgap!(fig.layout, 3, 5)
+    colgap!(fig.layout, 1, 5)
+    colgap!(fig.layout, 2, 0)
+    colgap!(fig.layout, 3, 25)
+
+    rowgap!(ylabellayout, 1, 7)
+    colgap!(xlabellayout, 1, 7)
+    rowgap!(legend_layout, 1, 5)
+
+    # colgap!(fig.layout, 1, 5)
+    # rowgap!(fig.layout, 3, 5)
+
     resize_to_layout!(fig)
 
-    display(fig)
+    save("figures/02_dS_dI_1.png", fig; px_per_unit = 10)
 
-    save("figures/02_dS_dI.png", fig; px_pet_unit = 4)
+    display(fig)
 
     nothing
 end
